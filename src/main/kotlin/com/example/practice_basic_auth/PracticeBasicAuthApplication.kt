4186818977eb164f14e21fd26f98c344b1592736
fjsho <81564274+fjsho@users.noreply.github.com>
2024-09-29 09:01:19 +0900

    initial commit

---
+++ src/main/kotlin/com/example/practice_basic_auth/PracticeBasicAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.example.practice_basic_auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeBasicAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeBasicAuthApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/practice_basic_auth/PracticeBasicAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.practice_basic_auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeBasicAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
